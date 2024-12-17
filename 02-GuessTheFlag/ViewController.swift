005a3ec2ca50c4f51369dda0daf2fe7177293d86
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-12-17 16:12:19 -0300

    create a Guess the flag app

--- 02-GuessTheFlag/ViewController.swift
+++ 02-GuessTheFlag/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        buttonOne.layer.borderWidth = 1
+        buttonOne.layer.borderColor = UIColor.lightGray.cgColor
+        
+        buttonTwo.layer.borderWidth = 1
+        buttonTwo.layer.borderColor = UIColor.lightGray.cgColor
+        
+        buttonThree.layer.borderWidth = 1
+        buttonThree.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
-
-
+    
+    private func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
+        buttonTwo.setImage(UIImage(named: countries[1]), for: .normal)
+        buttonThree.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Wrong!"
+            score -= 1
+        }
+        
+        let alertController = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(alertController, animated: true, completion: nil)
+    }
 }
-
